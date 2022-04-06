c50fa8c882f174a36573a0563b7fb6bc50db7311
ruljaka <ruljaka@gmail.com>
2022-04-06 14:03:33 +0300

    Lesson 2 : 2.2 'Homework'

--- app/src/main/java/com/ruslangrigoriev/homework22_activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/ruslangrigoriev/homework22_activitylifecycle/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.ruslangrigoriev.homework22_activitylifecycle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_COUNT = "extra.COUNT"
+    }
+
+    private var count = 0
+    private var tvCountShow: TextView? = null
+    private var btnCount: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnCount = findViewById(R.id.btn_count)
+        tvCountShow = findViewById(R.id.tv_count_show)
+
+        btnCount?.setOnClickListener {
+            count++
+            tvCountShow?.text = count.toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(EXTRA_COUNT, count)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        count = savedInstanceState.getInt(EXTRA_COUNT)
+        tvCountShow?.text = count.toString()
     }
 }
